01c2988e915bdc9dd7f6145031302054c543ea76
Aleš Ružić <ruzic888@gmail.com>
2024-04-23 22:56:46 +0200

    Init
    Initial commit

---
+++ movieApp/src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-img
+          src="https://files.readme.io/29c6fee-blue_short.svg"
+          width="40px"
+          height="40px"
+        />
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup() {
+    return {};
+  },
+});
+</script>
